642fc62eaa4f7506986e6c579046bab1cacc4d22
Jose Ignacio Pareja <jose.ignacio97@gmail.com>
2022-04-21 20:04:17 -0300

    Changed createProject, Added addingSelector.

--- src/app.js
+++ src/app.js
@@ -2,31 +2,47 @@ import './style.css';
 import createProject from './createProject';
 import createTodo from './createTodo';
 
+// INPUTS
 const myProjects = [];
+const myTodos = [];
+const projectname = document.querySelector('#projectinput');
+const maincontent = document.querySelector('.maincontent');
 
+const todotitle = document.querySelector('#todotitle');
+const descrip = document.querySelector('#description');
+const date = document.querySelector('#date');
+const priority = document.querySelector('#priority');
+const selectProject = document.querySelector('#selectProject');
+
+
+// createProject
 const projectbtn = document.querySelector('#projectbtn');
 projectbtn.addEventListener('click', () => {
-    const projectname = document.querySelector('#projectinput');
     const newproject = createProject(projectname.value);
     myProjects.push(newproject);
-    const maincontent = document.querySelector('.maincontent');
-    const aqua = document.createElement('span');
-    aqua.textContent = myProjects[myProjects.length - 1]['projectname'];
-    maincontent.appendChild(aqua);
+    const span = document.createElement('span');
+    span.textContent = myProjects[myProjects.length - 1]['projectname'];
+    span.classList.add(myProjects[myProjects.length - 1]['projectname']);
+    maincontent.appendChild(span);
 });
 
+
+// addingSelector
+projectbtn.addEventListener('click', () => {
+    const option = document.createElement('option');
+    option.textContent = projectname.value;
+    option.value = projectname.value;
+    selectProject.appendChild(option);
+});
+
+
+// createTodo
 const addbtn = document.querySelector('#addbtn');
 addbtn.addEventListener('click', () => {
-    // Form Inputs:
-    const todotitle = document.querySelector('#todotitle');
-    const descrip = document.querySelector('#description');
-    const date = document.querySelector('#date');
-    const priority = document.querySelector('#priority');
-    const selectProject = document.querySelector('#selectProject');
-    // Form Button:
     const newtodo = createTodo(todotitle.value, descrip.value, date.value, priority.value, selectProject.value);
-    if (newtodo.projectselector.value ===  aqua.textContent) {
-        aqua.appendChild(newtodo);
-    } else console.log("No Project Found");
-    //
+    myTodos.push(newtodo);
+    const adiv = document.createElement('div');
+    adiv.classList.add('todo');
+    console.log(maincontent);
+    //seleccionar .appendChild(adiv);
 });
